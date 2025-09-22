7fd857cf3b6b9f3528e25d3e493ab5804e36d49c
ChiCheng <cc610649834@163.com>
2025-09-22 09:14:00 +0800

    UPDATE2025.9.21

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="RL-WaterLily",  # 包名称
     version="0.1.0",          # 版本号
     description="RL-WaterLiy: a simulation framework for RL in fluid gynamics",     # 简短描述
-    long_description=open("README.md").read(),  # 详细描述（通常从README读取）
+    long_description=open("README.md", encoding="utf-8").read(),  # 详细描述（通常从README读取）
     long_description_content_type="text/markdown",  # README格式
     author="Chi Cheng, Aoming Liang, Boai Sun, Yuqi Yan, Dixia Fan",
     author_email="cc610649834@163.com",
