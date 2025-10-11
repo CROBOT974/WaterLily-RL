b861c4e0287e3c60c8a846464b001e056610b0bc
ChiCheng <cc610649834@163.com>
2025-10-11 12:37:29 +0800

    UPDATE2025.10.10

--- src/test.py
+++ src/test.py
@@ -16,21 +16,16 @@ from gym_base import JuliaEnv
 Lib支持
 
 """
-import sys
 import numpy as np
-import torch.nn as nn
-from gymnasium.wrappers import RescaleAction
 from stable_baselines3 import PPO
-from stable_baselines3 import SAC
-from stable_baselines3.common.utils import set_random_seed
-from stable_baselines3.common.vec_env import DummyVecEnv, SubprocVecEnv
+from stable_baselines3.common.vec_env import DummyVecEnv
 from stable_baselines3.common.callbacks import BaseCallback, CheckpointCallback, CallbackList
-import imageio
 import os
+from src.gif import create_GIF
 # dir videos
 video_folder = "./videos/"
 os.makedirs(video_folder, exist_ok=True)
-savefile = os.path.join("./model",)
+
 
 """
 
@@ -72,7 +67,7 @@ checkpoint_callback = CheckpointCallback(
     save_vecnormalize=True
 )
 
-def learn(env_name = "VIV-v0", save_file = "PPO_model", total_timesteps = 80_000):
+def learn(env_name, save_file, total_timesteps = 80_000):
     """
 
     单线程环境建立，训练，保持(注册表)
@@ -89,17 +84,50 @@ def learn(env_name = "VIV-v0", save_file = "PPO_model", total_timesteps = 80_000
     reward_callback = RewardLoggerCallback()
     callback = CallbackList([checkpoint_callback, reward_callback])
     model.learn(total_timesteps=total_timesteps, callback = callback)
-    model.save(save_file)
+    model.save(os.path.join("./model",save_file))
     rewards = np.array(reward_callback.episode_rewards)
     env.close()
+
+def play(env_name, load_file, gif_file, data_file):
+    # same simulation env while 'render_mode' is 'rgb_array' to create images
+    env = gym.make(env_name)
+
+    # load the trained PPO_model
+    model = PPO.load(os.path.join("./model",load_file), env=env)
+
+    # reset the env
+    # print("测试",env.reset())
+    obs, _ = env.reset()
+
+    done = False
+    truncated = False
+
+    # if 'not done', then continue to perform the simulation operation based on trained model
+    while not done and not truncated:
+        action, _ = model.predict(obs, deterministic=True)
+        obs, reward, done, truncated, info = env.step(action)
+
+    # save as gif
+    input_frame = "images"
+    output_gif = os.path.join("./result/gif",gif_file)
+    create_GIF(input_frame, output_gif)
+    env.close()
+
+    # save the info
+    np.save(os.path.join("./result/data",data_file), info["info"])
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--learn', action='store_true', help='play or learn.')
+    parser.add_argument('--play', action='store_true', help='play or learn.')
     parser.add_argument('--env_name', type=str, default='VIV-v0', help='environment name.')
     parser.add_argument('--filename', type=str, default='PPO_model', help='filename to save/load model.')
+    parser.add_argument('--giffilename', type=str, default='train_policy_demo.gif', help='filename to save/load gif.')
+    parser.add_argument('--datafilename', type=str, default='info_PPO.npy', help='filename to save/load data.')
     parser.add_argument('--total_timesteps', type=int, default=80_000, help='total timesteps.')
     args = parser.parse_args()
-
-    learn(args.env_name, args.filename, args.total_timesteps)
+    if args.play:
+        print("开始测试")
+        play(args.env_name, args.filename, args.giffilename, args.datafilename)
+    else:
+        learn(args.env_name, args.filename, args.total_timesteps)
